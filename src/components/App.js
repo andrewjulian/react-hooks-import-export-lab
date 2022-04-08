6fb83bf413a2751a5579fa7569e8cdd5468e3316
+ <+>
2022-04-08 14:14:25 -0600

    commit tree 1c2c057ee2b43e690c484e8d0091e949ffe8e3fd

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "/Users/andrewsm1/Development/code/phase-2/react-hooks-import-export-lab/src/components/NavBar.js";
+import Home from "/Users/andrewsm1/Development/code/phase-2/react-hooks-import-export-lab/src/components/Home.js";
+import About from "/Users/andrewsm1/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js";
 
 function App() {
   return (
